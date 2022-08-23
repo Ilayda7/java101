fd4947fa28edf4b5b3ca1b7c619b5c2cc50f9704
ilayda arslan <ilaydarsln@icloud.com>
2022-08-23 15:37:33 +0300

    first commit

--- ilo/src/Ilo2.java
+++ ilo/src/Ilo2.java
@@ -1,5 +1,9 @@
+
 public class Ilo2 {
     public static void main(String[] args) {
-        System.out.print("atahan");
+        byte b = 0;
+        int a = b++;
+        System.out.println(a);
+
     }
 }
