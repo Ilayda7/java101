4f51ddb5e9aaec6648f6562346160c3bd4310d45
ilayda arslan <ilaydarsln@icloud.com>
2022-08-31 23:03:54 +0300

    first commit

---
+++ VucutKitleEndeksi/src/VucutKitleEndeksi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VucutKitleEndeksi {
+    public static void main(String[] args) {
+
+        double boy, kilo, indeks;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Lütfen boyunuzu metre cinsinde giriniz : ");
+        boy = inp.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz : ");
+        kilo = inp.nextDouble();
+
+        indeks = (kilo / (boy * boy));
+        System.out.println("Vücut Kitle İndeksiniz : " +indeks);
+
+
+
+    }
+
+
+
+}
